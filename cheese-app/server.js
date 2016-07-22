80a7ecb7ec3b4a01afba26ec277c1898eb60c965
Harry Ho <TheTitan8706@gmail.com>
2016-07-22 15:34:58 -0400

    feat(node): adds node application to dockerize

--- cheese-app/server.js
+++ cheese-app/server.js
@@ -2,28 +2,17 @@ var http = require('http');
 var koa = require('koa');
 var app = koa();
 
+//  Sets the PORT to the env variable or default
 var PORT = process.env.PORT ? process.env.PORT : 3001;
-if (process.env.PORT) {
-  console.log('Using Port', process.env.PORT);
-}
 
-if (process.env.CONTAINER_NAME) {
-  console.log('ContainerName: ', process.env.CONTAINER_NAME);
-}
-
-app.use(function *(){
+//  Simple Use function for return
+app.use(function *() {
   this.type = 'application/json';
   this.body = {
-    message : 'Hello World',
+    message : 'I like to eat cheese',
     port: PORT
   };
-  if (process.env.CONTAINER_NAME) {
-    this.body.container = process.env.CONTAINER_NAME;
-  }
 });
 
-app.on('error', function () {
-  console.log('Error occurred');
-});
-
+//  Creates a simple server
 http.createServer(app.callback()).listen(PORT);
